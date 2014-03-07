c6c3a3b72e31c42878c79d3a0f9b38c138cf504e
Vladimir Grinenko <i@tadatuta.com>
2014-03-08 00:02:40 +0200

    Move level config to make.js

--- .bem/make.js
+++ .bem/make.js
@@ -1,7 +1,9 @@
 /* jshint node:true */
 /* global MAKE */
 
-var environ = require('bem-environ')(__dirname);
+var PATH = require('path'),
+    environ = require('bem-environ')(__dirname);
+
 environ.extendMake(MAKE);
 
 //process.env.YENV = 'production';
@@ -9,10 +11,10 @@ environ.extendMake(MAKE);
 
 MAKE.decl('Arch', {
 
-    blocksLevelsRegexp: /^.+?\.blocks/,
-    bundlesLevelsRegexp: /^.+?\.bundles$/,
+    blocksLevelsRegexp : /^.+?\.blocks/,
+    bundlesLevelsRegexp : /^.+?\.bundles$/,
 
-    libraries: [
+    libraries : [
         'bem-core @ f4b46ef0590549042d938f7e981df4d14eb4caef',
         'bem-components @ 82301a8af6c15c2849d1f755a24f594de6522251'
     ]
@@ -22,7 +24,7 @@ MAKE.decl('Arch', {
 
 MAKE.decl('BundleNode', {
 
-    getTechs: function() {
+    getTechs : function() {
 
         return [
             'bemjson.js',
@@ -35,6 +37,28 @@ MAKE.decl('BundleNode', {
             'html'
         ];
 
+    },
+
+    getLevelsByBundleName : function() {
+        return {
+            'desktop.bundles' : [
+                'libs/bem-core/common.blocks',
+                'libs/bem-core/desktop.blocks',
+                'libs/bem-components/common.blocks',
+                'libs/bem-components/desktop.blocks',
+                'common.blocks',
+                'desktop.blocks'
+            ]
+        };
+    },
+
+    getLevels : function() {
+        var resolve = PATH.resolve.bind(PATH, this.root),
+            buildLevel = this.getLevelPath();
+
+        return (this.getLevelsByBundleName()[buildLevel] || [])
+            .map(function(path) { return resolve(path); })
+            .concat(resolve(PATH.dirname(this.getNodePrefix()), 'blocks'));
     }
 
 });
